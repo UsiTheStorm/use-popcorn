9b905d2bdb44196de6cf941f608fa5d7668b2a19
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-03 10:44:06 +0100

    Cleanup

--- src/App.jsx
+++ src/App.jsx
@@ -140,37 +140,11 @@ export default function App() {
 
   return (
     <>
-      <div>
-        <TextExpander>
-          Space travel is the ultimate adventure! Imagine soaring past the stars and exploring new
-          worlds. It's the stuff of dreams and science fiction, but believe it or not, space travel
-          is a real thing. Humans and robots are constantly venturing out into the cosmos to uncover
-          its secrets and push the boundaries of what's possible.
-        </TextExpander>
-
-        <TextExpander
-          collapsedNumWords={20}
-          expandButtonText="Show text"
-          collapseButtonText="Collapse text"
-          buttonColor="#ff6622"
-        >
-          Space travel requires some seriously amazing technology and collaboration between
-          countries, private companies, and international space organizations. And while it's not
-          always easy (or cheap), the results are out of this world. Think about the first time
-          humans stepped foot on the moon or when rovers were sent to roam around on Mars.
-        </TextExpander>
-
-        <TextExpander expanded={true} className="expanded-box">
-          Space missions have given us incredible insights into our universe and have inspired
-          future generations to keep reaching for the stars. Space travel is a pretty cool thing to
-          think about. Who knows what we'll discover next!
-        </TextExpander>
-      </div>
       {/* <StarRating maxRating={7} defaultRating={3} />
       <StarRating maxRating={10} size={24} color="red" /> */}
-      {/* <Navbar movies={movies} /> */}
+      <Navbar movies={movies} />
 
-      {/* <main className="main">
+      <main className="main">
         <Box>
           <MovieList movies={movies} />
         </Box>
@@ -186,7 +160,7 @@ export default function App() {
             </ul>
           </>
         </Box>
-      </main> */}
+      </main>
     </>
   );
 }

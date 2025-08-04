9b20b6d1099bcf5689919c6e3be42d056274057b
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-04 14:07:44 +0100

    Implement detail close when double click

--- src/App.jsx
+++ src/App.jsx
@@ -149,7 +149,7 @@ export default function App() {
   const [selectedId, setSelectedId] = useState(null);
 
   function handelSelectedMovie(id) {
-    setSelectedId(id);
+    setSelectedId((selectedId) => (id === selectedId ? null : id));
   }
 
   function handleCloseMovie() {

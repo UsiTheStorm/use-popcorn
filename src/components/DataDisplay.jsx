fdc640708a50200e9b37a17d821cae1b47d55130
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-05 11:19:25 +0100

    Create Data display helper component

---
+++ src/components/DataDisplay.jsx
@@ -0,0 +1,12 @@
+import React from 'react';
+
+import Loader from './Loader';
+import ErrorMessage from './ErrorMessage';
+
+function DataDisplay({ isLoading, error, children }) {
+  if (isLoading) return <Loader />;
+  if (error) return <ErrorMessage message={error} />;
+  return children;
+}
+
+export default DataDisplay;

9b7ffe5eaca48b282f9e18c75be7e74e5a1565a2
Usi the Storm <50480382+UsiTheStorm@users.noreply.github.com>
2025-08-12 15:35:03 +0100

    Add storing in local storage

--- src/App.jsx
+++ src/App.jsx
@@ -56,6 +56,7 @@ export default function App() {
 
   function handleAddWatched(movie) {
     setWatched((watched) => [...watched, movie]);
+    localStorage.setItem('watched', JSON.stringify([...watched, movie]));
   }
 
   function handleDeleteWatched(id) {
